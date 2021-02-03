1bfd54aa489d365e2b5a005d8e370661ab4f1c3b
jes0613 <46938121+jes0613@users.noreply.github.com>
2021-02-03 12:40:41 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Assignment_3_Josiah_Sarles.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_3_Josiah_Sarles.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        public IActionResult MoviesForm()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/AddMovie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_3_Josiah_Sarles.Models
+{
+    public class AddMovie
+    {
+
+        public string category { get; set; }
+
+        public string title { get; set; }
+
+        public int year { get; set; }
+
+        public string director { get; set; }
+
+        public string rating { get; set; }
+
+        public string edited { get; set; }
+
+        public string lent { get; set; }
+
+        public string notes { get; set; }
+
+    }
+}
